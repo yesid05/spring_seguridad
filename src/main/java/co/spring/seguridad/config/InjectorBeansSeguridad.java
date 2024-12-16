c046e9535088baf47bce320d1fbd33fecbd15769
Yesid05 <caicedopacho@hotmail.com>
2024-12-16 14:51:19 -0500

    Creando AuthenticationProvider

--- src/main/java/co/spring/seguridad/config/InjectorBeansSeguridad.java
+++ src/main/java/co/spring/seguridad/config/InjectorBeansSeguridad.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
@@ -17,6 +19,18 @@ public class InjectorBeansSeguridad {
 	public AuthenticationManager authenticationManager() throws Exception {
 		
 		return authenticationConfiguration.getAuthenticationManager();
+		
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		
+		DaoAuthenticationProvider estrategiaAutenticacion = new DaoAuthenticationProvider();
+		
+		estrategiaAutenticacion.setPasswordEncoder(null);
+		estrategiaAutenticacion.setUserDetailsService(null);
+		
+		return estrategiaAutenticacion;
 		
 	}
 	
